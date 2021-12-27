6e049b1d00c4e123a2e5e28fb5ac17d61183b9c4
zaki <natourzaki@gmail.com>
2021-12-27 12:03:19 +0700

    smart contract done and tested localy on ganache

--- test/Donation.js
+++ test/Donation.js
@@ -1,4 +1,3 @@
-
 const { web3 } = require("hardhat");
 require("chai").use(require("chai-bignumber")(web3.bignumber)).should();
 
@@ -20,14 +19,14 @@ contract("Donation", (accounts) => {
     it("accepts donation and checking the balance of the contract", async  ()=> {
       await donationcontract.gatherDonation({from:new_Donater, value: String(donationValue)});//donate with 1 ether
       const balance =await web3.eth.getBalance(donationcontract.address);
-      assert.equal(donationValue,balance,'wrong balance')
+      assert.equal(donationValue,parseInt(balance),'wrong balance');
     });
 
     it("checks the saving address of the donater and the amount that he donated with", async  ()=> {
-      const donaters=await donationcontract.getDonators()
-      assert.equal(new_Donater,donaters[0],'error in saving the address of donater')
+      const donaters=await donationcontract.getDonators();
+      assert.equal(new_Donater,donaters[0],'error in saving the address of donater');
       const donationValueForAddress =await  donationcontract.getDonationValueForAddress(donaters[0]);
-      assert.equal(donationValue,donationValueForAddress,"the stored  donation value doesn't agree with the donation")
+      assert.equal(donationValue,donationValueForAddress,"the stored  donation value doesn't agree with the donation");
     });
 
     it("restricts donaters from transfering the contract's balance to himself", async  ()=> {
@@ -35,38 +34,35 @@ contract("Donation", (accounts) => {
           await donationcontract.transferToOwner({from: new_Donater});
           assert(false);
         } catch (error) {
-          assert(error)
+          assert(error);
       }
-
     });
+
     it("restricts donaters from transfering the contract's balance to  another public address", async  ()=> {
       try {
           await donationcontract.transferToAddress(accounts[2],{from: new_Donater});
           assert(false);
         } catch (error) {
-          assert(error)
+          assert(error);
       }
-
     });
 
     it("allows owner to transfer the balance of the contract to himself", async  ()=> {
       await donationcontract.transferToOwner({from: accounts[0]});
       const balance =await web3.eth.getBalance(donationcontract.address);
-      assert.equal(0,balance,"balance hasn't transfered to owner")
+      assert.equal(0,parseInt(balance),"balance hasn't transfered to owner");
     });
 
     it("accepts donation for the second time  and checking the balance of the contract", async  ()=> {
       await donationcontract.gatherDonation({from:new_Donater, value: String(donationValue)});//donate with 1 ether
       const balance =await web3.eth.getBalance(donationcontract.address);
-      assert.equal(donationValue,balance,'wrong balance')
+      assert.equal(donationValue,parseInt(balance),'wrong balance');
     });
 
     it("allows owner to transfer the balance of the contract to public address", async  ()=> {
       await donationcontract.transferToAddress(accounts[2],{from: accounts[0]});
       const balance =await web3.eth.getBalance(donationcontract.address);
-      assert.equal(0,balance,"balance hasn't transfered to owner")
+      assert.equal(0,parseInt(balance),"balance hasn't transfered to owner");
     });
-
-
 });
 
