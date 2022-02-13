ab37fbea63084cbc3531a3fc80d2207bff3461f9
zaki <natourzaki@gmail.com>
2022-02-13 13:51:39 +0700

    refactor the code in smart contract

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -15,36 +15,40 @@ contract Donation {
   }
   
 
-//save the addresses to a mapper
-  address[] private donators; 
-  mapping (address => bool) public Addresses;
+  address[] private donators;
+
+  struct DonaterInf{
+    uint amount;
+    bool exist;
+  }
+  mapping (address => DonaterInf ) private donationForAddress;
+
+
 
   function setAddress(address _address) public {
-        Addresses[_address]=true;
+        donationForAddress[_address].exist= true;
+        donators.push(_address);
   }
 
   function contains(address _address) public view returns (bool) {
-        return Addresses[_address];
+        return donationForAddress[_address].exist;
   }
 
-  //save the amount of every donation to the appropriate sender address
-  mapping (address => uint256 ) private donationForAddress;
 
 
   function gatherDonation() public payable {
     require(msg.value >= .001 ether, "too small donation");
     
     if (!contains(msg.sender)) {
-      setAddress(msg.sender);
-      donators.push(msg.sender);
+      setAddress(msg.sender); 
     }
 
-    donationForAddress[msg.sender]+=msg.value;
+    donationForAddress[msg.sender].amount+=msg.value;
   }
 
 
   function transferToAddress(address  recevier, uint256 amount) external ownerOnly {
-    require(amount <= getBalance(), "there is not enough balance, to trasfer this amount");
+    require(amount <= address(this).balance, "there is not enough balance, to trasfer this amount");
     payable(recevier).transfer(amount);
   
   }
@@ -54,11 +58,7 @@ contract Donation {
   }
 
   function getDonationValueForAddress(address donater) public view returns (uint256) {
-    return donationForAddress[donater];
-  }
-
-  function getBalance() public view returns (uint256) {
-         return address(this).balance;
+    return donationForAddress[donater].amount;
   }
 
 }
