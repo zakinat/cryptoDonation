138626de120262df627f569544d1b2066a329255
zaki <natourzaki@gmail.com>
2022-02-13 16:02:25 +0700

    refactor the task script

--- tasks/tasks.ts
+++ tasks/tasks.ts
@@ -6,22 +6,22 @@ dotenv.config();
 
 const provider = new ethers.providers.JsonRpcProvider(`https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`)
 
+
 async function main () {
+  const myPrivateKey= process.env.DEPLOYER_PRIVATE_KEY
 
-  const signer =await  new ethers.Wallet(`0x${process.env.DEPLOYER_PRIVATE_KEY}`, provider);
+  //const DonationStatus= await makeDonation(myPrivateKey, 0.001)
 
-  const Donation = await ethers.getContractFactory("Donation");
-    const donation = await Donation.attach("0x01804A4e70cAd7D7FC9878373edED2E8aAc62364");
 
-    const contractSigner = donation.connect(signer);
+  const donaters =await getDonaters(myPrivateKey)
 
-    //await donation.gatherDonation({ value: ethers.utils.parseEther(`${0.001}`)})
 
-    const donaters= await donation.getDonators()
-    
-    console.log(donaters)
-
-    console.log(await donation.getDonationValueForAddress(donaters[0]))
+  if (donaters[0]) {
+    const ethValue = await getDonation(myPrivateKey, donaters[0]);
+  }
+  
+  const addressReciver= `0x112f6b9C416BA1Fc9B650F82Ad269a182Ca9B0CC`;
+  //await transferDonation(myPrivateKey, addressReciver, 0.002);
 
 }
   
@@ -30,11 +30,93 @@ async function main () {
 
 
   main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+     .then(() => process.exit(0))
+     .catch(error => {
+       console.error(error);
+       process.exit(1);
+     });
 
 
+
+    async function getSmartContract(privateKey: any) {
+      const Donation = await ethers.getContractFactory("Donation");
+      const donation = await Donation.attach("0x01804A4e70cAd7D7FC9878373edED2E8aAc62364");
+   
+      const signer =await  new ethers.Wallet(`0x${privateKey}`, provider);
+      const contractSigner = donation.connect(signer);
+   
+      return donation
+    }
+   
+   
+    async function makeDonation(privateKey: any, value: any) {
+      const donation = await  getSmartContract(privateKey);
+   
+      try {
+        await donation.gatherDonation({ value: ethers.utils.parseEther(`${value}`) })
+        console.log('Donation Done!')
+        
+        return 'Donation Done!'
+   
+      } catch (error) {
+        console.log(error)
+   
+        return 'Donation Failed :('
+      }
+     
+    }
+
+    async function getDonaters(privateKey: any) {
+      const donation = await  getSmartContract(privateKey);
+   
+      try {
+        const donaters= await donation.getDonators()
+        console.log(donaters)
+        return donaters
+   
+      } catch (error) {
+        console.log(error)
+   
+        return 'get Donaters Failed :('
+      }
+     
+    }
+
+
+    async function getDonation(privateKey: any, address: any) {
+      const donation = await  getSmartContract(privateKey);
+   
+      try {
+        const weiValue= await donation.getDonationValueForAddress(address)
+
+        console.log(ethers.utils.formatEther(weiValue), ' ether')
+        
+        return ethers.utils.formatEther(weiValue)
+   
+      } catch (error) {
+        console.log(error)
+   
+        return 'get Donation Failed :('
+      }
+     
+    }
+
+
+    async function transferDonation(privateKey: any, address: any, amount: any) {
+      const donation = await  getSmartContract(privateKey);
+   
+      try {
+        await donation.transferToAddress(address, ethers.utils.parseEther(`${amount}`))
+
+        console.log('transfer donation done')
+        
+        return true
+   
+      } catch (error) {
+        console.log(error)
+   
+        return 'transfer Donation Failed :('
+      }
+     
+    }
 
